1edf3cd837f3565a36332d3b775251a779bb2a33
Leci1259 <lreid31@gatech.edu>
2021-11-16 10:49:07 -0500

    inital commit

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import navBar from './navigation';
+
+function headerDisplay() {
+    return (
+        <header>
+            <h1>Lesiana Reid</h1>
+            {navBar}
+        </header>
+    )
+}
