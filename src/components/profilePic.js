4ad5e7411c381bfb20559d4b6f15363811876b08
Leci1259 <lreid31@gatech.edu>
2021-11-17 21:51:08 -0500

    trying to fix image rendering

--- src/components/profilePic.js
+++ src/components/profilePic.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function AboutMe() {
     return (
-        <img src='/images/BioImage.jpg' alt="Bio Pic of Lesiana Reid" id="bio-img"></img>
+        <img src='./images/BioImage.jpg' alt="Bio Pic of Lesiana Reid" id="bio-img"></img>
     )
 }
