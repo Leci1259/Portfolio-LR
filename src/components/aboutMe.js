0a76a59a34f6a9d5eb2afd5833f6ef353c947deb
Leci1259 <lreid31@gatech.edu>
2021-12-03 11:03:11 -0500

    small changes

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -12,8 +12,7 @@ export default function Profilepic() {
                 <h3>
 
                     Innovative Web Developer with a Certificate in Full Stack Web Development from Georgia Institute of Technology. Bachelor degrees from Georgia Institute of Technology and Agnes Scott College.
-                    Skills in HTML, CSS, and JavaScript/Python and strengths in producing high quality applications, working in fast paced environments, and working in diverse multidisciplinary teams. Passionate about approaching programming challenges from different angles and collaborating with others to create meaningful web applications. Excited to develop responsive websites and provide clients with the highest satisfaction. Positioned to provide unique perspectives on how end users interact with websites and software platforms by leveraging background in industrial/organizational psychology, engineering psychology, and ergonomics.
-
+                    Skills in React, SQL/NoSQL, and JavaScript/Java/Python and strengths in producing high quality applications, working in fast paced environments, and working in diverse multidisciplinary teams. Passionate about approaching programming challenges from different angles and collaborating with others to create meaningful web applications. Excited to develop responsive websites and provide clients with the highest satisfaction. Positioned to provide unique perspectives on how end users interact with websites and software platforms by leveraging background in industrial/organizational psychology, engineering psychology, and ergonomics.
 
                 </h3>
             </div>
